0034a64f39d370e79d7db74c082281fd182504e8
Thiru02 <89373686+Thiru02@users.noreply.github.com>
2022-10-14 18:07:45 +0530

    Time: 5909 ms (5.01%), Space: 68 MB (5.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i in nums){
+        for (let j in nums){
+            if (i!==j & nums[i]+nums[j]===target){
+                return [i,j]
+            }
+        }
+    }
+};
