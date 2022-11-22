3a46c5f72b641410fa63667e502d11ab8677e750
Thiru02 <89373686+Thiru02@users.noreply.github.com>
2022-11-22 17:43:50 +0530

    Time: 95 ms (47.78%), Space: 41.8 MB (74.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,30 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let count=0
-    let count1=0
-    for (let i=0;i<s.length;i++){
-        if (s[i]===" "||i===0){
-
-            if (s[i+1]!==undefined&&s[i+1]!==" ")count=0
-                        if (i===0)j=i
-            else j=i+1
-            while (s[j]!==" "&&s[j]!==undefined){
-                count1++
-                count++
-                j++
-                i++
-            }
-        }
-    }
-        if (count1===0){
-        let x=0
-        for (let i of s){
-            if (i!==" "){
-                x++
-            }
-        }
-        return x
-    }
-    else return count
+        let trimmedString = s.trim();
+    
+    return trimmedString.length - trimmedString.lastIndexOf(' ') - 1;
 };
