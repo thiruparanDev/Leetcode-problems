e4ec74f28ec7f376d1ac07d20308e4dd24c38644
Thiru02 <89373686+Thiru02@users.noreply.github.com>
2022-11-22 14:12:59 +0530

    Time: 64 ms (92.32%), Space: 41.7 MB (92.70%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,16 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i=0;i<nums.length;i++){
-        if (nums[i]>=target){
-            return i
+        let max=nums.length-1
+    let min=0
+    while(min<=max){
+        let mid = Math.floor((min+max)/2)
+        if (target===nums[mid])return mid
+        if(target>nums[mid]) min=mid+1
+        else max=mid-1
+        if (max<min){
+            if (target>nums[mid]) return mid+1
+            else if (target<[nums[mid]]) return mid
         }
     }
-    return nums.length
 };
