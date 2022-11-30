1389d7a55edd35bb34f3fabbb9c8fe560bc6526a
Thiru02 <89373686+Thiru02@users.noreply.github.com>
2022-12-01 05:17:26 +0530

    Time: 53 ms (71.29%), Space: 13.9 MB (75.49%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,8 +1,27 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        quotient = int(dividend/divisor)
-        if quotient>=2**31-1:
+        a=abs(dividend)
+        b=abs(divisor)
+        tot_count=0
+        signA= 'p'
+        signB= 'p'
+        if dividend<0:
+            signA='n'
+        if divisor<0:
+            signB='n'
+        while b<=a:
+            count=1
+            dec=b
+            while dec<=a:
+                a-=dec
+                tot_count+=count
+                count+=count
+                dec+=dec
+        if (signA=='n' and signB=='p') or (signA=='p' and signB=='n'):
+            tot_count = -abs(tot_count)
+        if tot_count>=2**31-1:
             return 2**31-1
-        elif quotient<=pow(-2,31):
+        elif tot_count<=pow(-2,31):
             return pow(-2,31)
-        return quotient
+        else:
+            return tot_count
