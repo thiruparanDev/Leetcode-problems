0adec4141ca379520ca74164e22394050c51ea9c
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-05 03:55:15 +0530

    Time: 109 ms (54.46%), Space: 55.6 MB (58.42%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,31 +11,33 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let values = [];
-    let overflow = 0;
-    while (l1.next || l2.next) {
-        const value = (l1.val + l2.val + overflow) % 10;
-        overflow = l1.val + l2.val + overflow >= 10 ? 1 : 0;
-        values.push(value);
-
-        l1 = l1.next || new ListNode(0);
-        l2 = l2.next || new ListNode(0);
-    }
-    const value = (l1.val + l2.val + overflow) % 10;
-    overflow = l1.val + l2.val + overflow >= 10 ? 1 : 0;
-    values.push(value);
-    if (overflow === 1) {
-        values.push(1);
-    }
-    let node = undefined;
-    values.reverse().forEach(item => {
-        if (node === undefined) {
-            node = new ListNode(item);
-        } else {
-            const newNode = new ListNode(item);
-            newNode.next = node;
-            node = newNode;
+    const dummy = new ListNode()
+    let current = dummy
+    let carry = 0
+    
+    while (l1 || l2){
+        let val = 0
+        val += carry
+        if (l1){
+            val += l1.val
+            l1 = l1.next
         }
-    });
-    return node;
+        if (l2){
+            val += l2.val
+            l2 = l2.next
+        }
+        if (val>=10){
+            carry =1
+            val = val-10
+        }
+        else {
+            carry = 0
+        }
+        current.next = new ListNode(val)
+        current = current.next   
+    }
+    if (carry>0){
+        current.next = new ListNode(carry)
+    }
+    return dummy.next
 };
