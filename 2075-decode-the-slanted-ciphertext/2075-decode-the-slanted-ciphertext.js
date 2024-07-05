770df8bfe3df0a320a42dcf3353020f7ae7580a4
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-05 07:45:25 +0530

    Time: 344 ms (37.50%), Space: 98.4 MB (75.00%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} encodedText
+ * @param {number} rows
+ * @return {string}
+ */
+var decodeCiphertext = function(encodedText, rows) {
+    const letPerRow = encodedText.length/rows
+    let origText = ""
+    for (let j=1; j<= letPerRow; j++){
+        // let count = 0;
+        // let emptCount = 0;
+        for (let i=1; i<=rows; i++){
+        // count++;
+        pos = i+j + (i-1)*letPerRow - 2
+        if (encodedText[pos] === undefined){
+            break;
+        }
+        if (encodedText[pos] === ' '){
+            // emptCount++;
+        }
+        origText += encodedText[pos]
+        }
+        // if (count === emptCount){
+        //     origText = origText.substring(0, str.length-emptCount)
+        //     break;
+        // }
+    }
+    return origText.replace(/\s+$/, '')
+};
