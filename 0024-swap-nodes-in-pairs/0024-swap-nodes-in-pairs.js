bfd0da81370245069badac3282f8b1b5fc0c0850
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-06 10:20:46 +0530

    Time: 45 ms (89.75%), Space: 49.4 MB (10.63%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    // let final = head
+    // let current = head
+    // if (!current.next){
+    //     return
+    // }
+    // else {
+    //     let dummy = current.next.next
+    //     let dummy2 = current.next
+    //     current.next = dummy
+    //     dummy2.next = current
+    //     final = dummy2
+    //     if (final.next.next){
+    //         current = current.next.next
+    //     }
+    // }
+    let current  = head
+    const final = new ListNode()
+    let finalDummy = final
+    while (current){
+        if (current.next){
+            let dummy = current.next.next
+            let dummy2 = current.next
+            current.next = dummy
+            dummy2.next = current
+            finalDummy.next = dummy2
+            finalDummy = finalDummy.next.next
+            current = dummy2.next.next
+        }
+        else {
+            finalDummy.next = current
+            current = current.next
+        }
+
+    }
+    return final.next
+
+};
