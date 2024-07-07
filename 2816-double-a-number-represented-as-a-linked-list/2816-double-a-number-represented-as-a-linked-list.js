09fc9ffb6dec4006eecd24de3af259d2e601478c
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-07 07:35:11 +0530

    Time: 165 ms (94.22%), Space: 62.2 MB (31.53%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var doubleIt = function(head) {
+    const dummy = new ListNode(0)
+    let current = dummy
+//     let carry = ''
+//     if (head){
+//         carry += String(head.val)
+//     }
+    
+//     while (head.next !== null){
+//         head = head.next
+//         carry += String(head.val)
+//     }
+//     carry = String(Number(carry)*2)
+//     for (i=0; i<carry.length; i++){
+//         current.next = new ListNode(Number(carry[i]))
+//         current = current.next
+//     }
+//     return dummy.next
+    // let prev = null
+    let val = 0
+    while (head !== null){
+        val = 2*head.val
+        if (val>=10){
+            current.next = new ListNode(val-10)
+            current.val = current.val+1
+        }
+        else {
+            current.next = new ListNode(val)
+        }
+        // prev = current
+        current = current.next
+        head = head.next
+    }
+    if (dummy.val ===0){
+        return dummy.next
+    }
+    else {
+        return dummy
+    }
+};
