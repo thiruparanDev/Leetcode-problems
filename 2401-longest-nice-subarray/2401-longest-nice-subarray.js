26ae686a6a4e51728453629fc8f8f4b1b1617cfb
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-08 15:57:25 +0530

    Time: 57 ms (98.21%), Space: 58.8 MB (51.79%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.js
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.js
@@ -3,84 +3,19 @@
  * @return {number}
  */
 var longestNiceSubarray = function(nums) {
-    // let nice = 1
-    // let i=0
-    // let j=i+1
-    // let dummyj = j
-    // let elements = 2
-    // let fail = false
-    // while(j<nums.length){
-    //     while (dummyj<nums.length){
-    //         for (let l = i; l<j; l++){
-    //             for (let k=l; k<dummyj; k++){
-    //                 if (nums[k]&nums[j] !== 0){
-    //                     fail = true
-    //                     break;
-    //                 }
-    //             }
-    //             if (fail){
-    //                 break;
-    //             }
-    //         }
-    //         if (fail){
-    //             break;
-    //         }
-    //         else {
-    //             dummyj++
-    //             elements++
-    //         }
-    //     }
-    //     if (!fail){
-    //         nice = elements
-    //     }
-    //     else {
-    //         nice = elements-1
-    //     }
-    //     i++
-    //     j=i+1
-    //     elements = 2
-    //     dummyj = j
-    //     fail = false
-    // }
-    // return nice
-    
-    let nice = 1
-    let i=0
-    let j=i+1
-    let dummyj = j
-    let elements = 1
-    let fail = false
-    while(j<nums.length){
-        while (dummyj<nums.length){
-            elements++
-            for (let k=i; k<dummyj; k++){
-                if ((nums[k]&nums[dummyj]) !== 0){
-                    fail = true
-                    break;
-                }
-            }
-            if (fail){
-                break;
-            }
-            else {
-                dummyj++
-            }
+    let left = 0;
+    let currentMask = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        while ((currentMask & nums[right]) !== 0) {
+            currentMask ^= nums[left];
+            left++;
         }
-        if (!fail){
-            if (nice<elements){
-                nice = elements
-            }
-        }
-        else {
-            if (nice<(elements-1)){
-                nice = elements-1
-            }
-        }
-        i++
-        j=i+1
-        elements = 1
-        dummyj = j
-        fail = false
+        
+        currentMask |= nums[right];
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-    return nice
+
+    return maxLength;
 };
