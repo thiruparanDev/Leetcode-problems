c9a2a8b3ebc54d3ca576c92a2bc097235b5d9b24
thiruparanDev <89373686+thiruparanDev@users.noreply.github.com>
2024-07-28 17:45:28 +1000

    Time: 45 ms (100.00%), Space: 50.1 MB (84.33%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.js
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.js
@@ -4,19 +4,18 @@
  * @return {number}
  */
 var longestSubstring = function(s, k) {
-    const helper = (start, end)=>{
-        if (end - start < k) return 0;
-        const f = new Map()
+    const helper = (start, end)=>{
+        let f = new Map()
         for (let i=start; i<end; i++){
-            f.set(s[i],(f.get(s[i]) || 0)+1)
+            f.set(s[i],(f.get(s[i])||0)+1)
         }
         for (let m=start; m<end; m++){
             if (f.get(s[m])<k){
                 let mNext = m+1
-                while(f.get(s[mNext])<k&&mNext<end){
+                while(f.get(s[mNext])<k && mNext<end){
                     mNext++
                 }
-                return Math.max(helper(start,m),helper(mNext,end))
+            return Math.max(helper(start,m),helper(mNext,end))
             }
         }
         return end-start
